9851689c8491ac2a5897c95b0df1d62e600f3487
MagmaMusen <MagmaMusen@gmail.com>
2020-02-12 15:24:53 +0100

    Added new project with copied classes and interfaces

---
+++ ECS_Legacy_Refactored/Heater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_Legacy_Refactored
+{
+    class Heater
+    {
+        
+            public void TurnOn()
+            {
+                System.Console.WriteLine("Heater is on");
+            }
+
+            public void TurnOff()
+            {
+                System.Console.WriteLine("Heater is off");
+            }
+
+            public bool RunSelfTest()
+            {
+                return true;
+            }
+    }
+}
